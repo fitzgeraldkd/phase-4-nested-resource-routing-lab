fd9970b63617dae44a74d36bf33ea5725f7487d6
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-27 10:59:55 -0500

    commit tree f2e176d211921f7aafcd0ce51b75c50370fb10d5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_item_not_found
+  wrap_parameters format: []
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+      render json: items
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_item_not_found(error)
+    render json: error, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
